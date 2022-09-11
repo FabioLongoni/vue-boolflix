da7ae81a8cebde11aef197c744604d3f3420265b
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-11 11:54:38 +0200

    check axios

--- src/App.vue
+++ src/App.vue
@@ -4,47 +4,62 @@
       <li v-for="movie in movies" :key="movie.id">
         {{ movie.original_title }}
       </li>
+      <li v-for="serie in tvSeries" :key="serie.id">
+        {{ serie.name}}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-
-
-import axios from 'axios';
-
-export default {
-  name: 'App',
-  components: {
-    
-  },
-  data() {
-    return {
-      movies: [],
-      api_key: 'ab0c47b7b4ecf2838075c86d8541abac',
-      api_URL: 'https://api.themoviedb.org/3',
-      query: 'ritorno'
-    }
-  },
-  methods: {
-    getMovies() {
-      axios.get(`${this.api_URL}/search/movie?`,{
-        params: {
-          api_key: this.api_key,
-          query: this.query
-        }
-      })
-      .then((res) => {
-        console.log(res);
-        this.movies = res.data.results;
-      })
+  import axios from 'axios';
+  
+  export default {
+    name: 'App',
+    data() {
+      return {
+        movies: [],
+        tvSeries: [],
+        api_key: 'ab0c47b7b4ecf2838075c86d8541abac',
+        query: 'pippo',
+        api_URI: 'https://api.themoviedb.org/3'
+      }
+    },
+    methods: {
+      getMovies() {
+        axios.get(`${this.api_URI}/search/movie`,{
+          params: {
+            api_key: this.api_key,
+            query: this.query
+          }
+        })
+        .then((res) => {
+          console.log(res.data.results)
+          this.movies = res.data.results;
+        })
+      },
+      getTvSeries() {
+        axios.get(`${this.api_URI}/search/tv`,{
+          params: {
+            api_key: this.api_key,
+            query: this.query
+          }
+        })
+        .then((res) => {
+          console.log(res.data.results)
+          this.tvSeries = res.data.results;
+        })
+      }
     },
     beforeMount() {
-      this.getMovies
-    },
+      this.getMovies()
+      this.getTvSeries()
+    }
   }
-}
 </script>
+
+
+
 
 <style lang="scss">
 #app {
